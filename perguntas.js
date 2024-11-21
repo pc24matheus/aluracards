0d02b5557f76b242effde5539a239a6562d8c82a
pc24matheus <00001116526621SP@al.educacao.sp.gov.br>
2024-11-21 13:54:12 -0300

    perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'adivinha',
+    'por que a plantinha nao foi atendida no hospital?',
+    'porque só tinha medico de plantão'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'adivinha',
+    'quais as duas bolas que todo homem tem',
+    'os olhos'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'adivinha',
+    'quem veio primeiro o ovo ou a galinha?',
+    '??? kakakkkakakakakkakakkakakakkakkkakkkaka'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'adivinha',
+    'oq é um ponto no meio do nada?',
+    'fiofó do boi'
 )
